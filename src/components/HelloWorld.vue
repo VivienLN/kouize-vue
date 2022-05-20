9fc047b2005213aa755f704927652eae11c76ff9
VivienLN <vivienleneez@gmail.com>
2022-05-20 23:34:08 +0200

    POC chat messages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,37 @@
-<script setup>
+<script>
 import { ref } from 'vue'
 import CONFIG from '../config'
 import Chat from '../utils/chat'
 
-defineProps({
-  msg: String
-})
+export default {
+  data() {
+    return {
+      config: {},
+      messages: [],
+    }
+  },
 
-const count = ref(0)
-const lastMessage = '';
-
-
+  mounted() {
+    this.config = CONFIG;
+    Chat.onAnswer((user, message) => {
+      this.messages.push({user: user, message: message});
+      console.log('chat callback, message: ' + message);
+    });
+  }
+}
 
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>Kouize</h1>
 
-  <h2>Last message from {{ CONFIG.channel }}:</h2>
-  <p>{{ lastMessage }}</p>
+  <h2>Last message from {{ config.channel }}:</h2>
+  <ul>
+    <li v-for="{user, message} in messages">
+        <strong>{{ user }}: </strong>
+        {{ message }}
+    </li>
+  </ul>
 
   <p>
     Recommended IDE setup:
@@ -35,7 +48,6 @@ const lastMessage = '';
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,15 +1,13 @@
 
 import CONFIG from '../config'
 
-ComfyJS.onChat = (user, message, flags, self, extra) => {
-    console.log('onAnswer | Message: ' + message);
-}
 ComfyJS.Init(CONFIG.channel);
 
 export default {
-    // onAnswer: callback => {
-    //     ComfyJS.onChat = (user, message, flags, self, extra) => {
-    //         console.log('onAnswer | Message: ' + message);
-    //     }
-    // }
+    onAnswer(callback) {
+        ComfyJS.onChat = (user, message, flags, self, extra) => {
+            console.log('onAnswer | Message: ' + message);
+            callback(user, message);
+        }
+    }
 };
