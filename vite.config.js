50ff49da232798392ceb9a65cffa2f3ca5866bb4
VivienLN <vivienleneez@gmail.com>
2022-06-08 20:42:10 +0200

    add option in vite config to not remove CNAME file in dist when building

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ command, mode }) => ({
   plugins: [vue()],
+  build: {
+    emptyOutDir: false,
+  },
   server: {
     host: true,
     port: 3000,
